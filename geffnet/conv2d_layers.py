9a212f6bb9d6ea6342736f9ab108c30e1d8b7dff
Bassant Medhat <85830264+bassantmedhat@users.noreply.github.com>
2024-07-04 17:16:33 +0300

    Update conv2d_layers.py

--- geffnet/conv2d_layers.py
+++ geffnet/conv2d_layers.py
@@ -140,14 +140,14 @@ def create_conv2d_pad(in_chs, out_chs, kernel_size, **kwargs):
     padding = kwargs.pop('padding', '')
     kwargs.setdefault('bias', False)
     padding, is_dynamic = get_padding_value(padding, kernel_size, **kwargs)
-    if is_dynamic:
-        if is_exportable():
-            assert not is_scriptable()
-            return Conv2dSameExport(in_chs, out_chs, kernel_size, **kwargs)
-        else:
-            return Conv2dSame(in_chs, out_chs, kernel_size, **kwargs)
-    else:
-        return nn.Conv2d(in_chs, out_chs, kernel_size, padding=padding, **kwargs)
+    # if is_dynamic:
+    #     if is_exportable():
+    #         assert not is_scriptable()
+    #         return Conv2dSameExport(in_chs, out_chs, kernel_size, **kwargs)
+    #     else:
+    #         return Conv2dSame(in_chs, out_chs, kernel_size, **kwargs)
+    # else:
+    return nn.Conv2d(in_chs, out_chs, kernel_size, padding=padding, **kwargs)
 
 
 class MixedConv2d(nn.ModuleDict):
